20186aaf52341b2cdd308e8a3e6cb7a1730a38c3
martine franquinet <martine.franquinet@ulb.be>
2021-10-18 13:24:33 +0200

    commit1

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>Mon super site</title>
+</head>
+
+<!-- L'en-tête -->
+<header>
+</header>
+<!-- Le menu -->
+<nav id="menu">
+<div class="element_menu">
+<h3>Titre menu</h3>
+<ul>
+<li><a href="page1.html">Lien</a></li>
+<li><a href="page2.html">Lien</a></li>
+<li><a href="page3.html">Lien</a></li>
+</ul>
+</div>
+</nav>
+<!-- Le corps -->
+<div id="corps">
+<h1>Mon super site</h1>
+<p>
+Bienvenue sur mon super site !<br />
+
+</p>
+</div>
+<!-- Le pied de page -->
+<footer id="pied_de_page">
+<p>Copyright moi, tous droits réservés</p>
+</footer>
+</body>
+</html>
