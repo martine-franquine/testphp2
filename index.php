27957212002ce6dee4c67e056394924709488707
martine franquinet <martine.franquinet@ulb.be>
2021-10-19 14:37:31 +0200

    commit2

--- index.php
+++ index.php
@@ -4,32 +4,20 @@
 <meta charset="utf-8" />
 <title>Mon super site</title>
 </head>
-
-<!-- L'en-tête -->
-<header>
-</header>
-<!-- Le menu -->
-<nav id="menu">
-<div class="element_menu">
-<h3>Titre menu</h3>
-<ul>
-<li><a href="page1.html">Lien</a></li>
-<li><a href="page2.html">Lien</a></li>
-<li><a href="page3.html">Lien</a></li>
-</ul>
-</div>
-</nav>
+<body>
+<?php include("entete.php"); ?>
+<?php include("menus.php"); ?>
 <!-- Le corps -->
 <div id="corps">
 <h1>Mon super site</h1>
 <p>
 Bienvenue sur mon super site !<br />
-
+Vous allez adorer ici, c'est un site génial qui va
+parler de... euh... Je cherche encore un peu le thème de mon site.
+:-D
 </p>
 </div>
 <!-- Le pied de page -->
-<footer id="pied_de_page">
-<p>Copyright moi, tous droits réservés</p>
-</footer>
+<?php include("pied_de_page.php"); ?>
 </body>
 </html>
