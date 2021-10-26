c50e51a88f96d273b1cbdca8c371353de9f748fb
martine franquinet <martine.franquinet@ulb.be>
2021-10-26 12:30:48 +0200

    Update config.php

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+
+$host-getenv=("MYSQL_SERVICE_HOST");
+$port-getenv=("MYSQL_SERVICE_PORT");
+$user=("databaseuser");
+$pass=("databasepassword");
+$db=("databasename");
+
+$con=mysqli_connect(host,$user,$pass,$db)$
+
+if (!$con)
+{
+	print("notconnected");
+}
+
+else
+{
+
+	echo("connected");
+
+}
+
+?>
